ba3b4d65864b766d5c7971741a9d540a14a8440c
Toby Sullivan <tobyjsullivan@gmail.com>
2015-09-13 11:59:14 -0700

    Add api versioning and rspec to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
   # Api definition
-  namespace :api, defaults: { format: :json },
-                  constraints: { subdomain: 'api' },
-                  path: '/' do
-    # We are going to list our resources here
+  namespace :api,
+              defaults: { format: :json },
+              constraints: { subdomain: 'api' },
+              path: '/' do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true)do
+      # We are going to list our resources here
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
