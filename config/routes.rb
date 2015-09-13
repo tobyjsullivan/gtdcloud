04450094ccd9fb4540455c72ee171116ce79c5ee
Toby Sullivan <tobyjsullivan@gmail.com>
2015-09-13 11:17:30 -0700

    Add api route to conf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' },
+                  path: '/' do
+    # We are going to list our resources here
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
